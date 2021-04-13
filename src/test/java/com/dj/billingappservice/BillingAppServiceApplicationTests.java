62979cc0f3b56e99270cc05b196436c6c5b3478c
Jagabandhu malik <jagabandhumallick@gmail.com>
2021-04-14 01:02:50 +0530

    initial setup

---
+++ src/test/java/com/dj/billingappservice/BillingAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.billingappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
